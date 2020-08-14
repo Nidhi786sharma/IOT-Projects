57d98f3fc23884be79c1208d75d64486b68635c5
Nidhi Sharma <49151319+Nidhi786sharma@users.noreply.github.com>
2020-08-14 19:22:44 +0530

    Voltage upload thingspeak

---
+++ Project7/voltage-upload.ino
@@ -0,0 +1,201 @@
+/*
+ Arduino --> ThingSpeak Channel via Ethernet
+ 
+ The ThingSpeak Client sketch is designed for the Arduino and Ethernet.
+ This sketch updates a channel feed with an analog input reading via the
+ ThingSpeak API (https://thingspeak.com/docs)
+ using HTTP POST. The Arduino uses DHCP and DNS for a simpler network setup.
+ The sketch also includes a Watchdog / Reset function to make sure the
+ Arduino stays connected and/or regains connectivity after a network outage.
+ Use the Serial Monitor on the Arduino IDE to see verbose network feedback
+ and ThingSpeak connectivity status.
+ 
+ Getting Started with ThingSpeak:
+ 
+   * Sign Up for New User Account - https://thingspeak.com/users/new
+   * Create a new Channel by selecting Channels and then Create New Channel
+   * Enter the Write API Key in this sketch under "ThingSpeak Settings"
+ 
+ Arduino Requirements:
+ 
+   * Arduino with Ethernet Shield or Arduino Ethernet
+   * Arduino 1.0+ IDE
+   
+  Network Requirements:
+   * Ethernet port on Router    
+   * DHCP enabled on Router
+   * Unique MAC Address for Arduino
+ 
+ Created: October 17, 2011 by Hans Scharler (http://www.nothans.com)
+ 
+ Additional Credits:
+ Example sketches from Arduino team, Ethernet by Adrian McEwen
+ 
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+int sensor_pin = A0;
+int output_value ;
+float sensor=A1;
+float gas_value;
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// Local Network Settings
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x30 }; // Must be unique on local network
+
+// ThingSpeak Settings
+char thingSpeakAddress[] = "api.thingspeak.com";
+String writeAPIKey = "IO0RXCP673IH09XM";
+const int updateThingSpeakInterval = 16 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+
+// Variable Setup
+long lastConnectionTime = 0; 
+boolean lastConnected = false;
+int failedCounter = 0;
+
+// Initialize Arduino Ethernet Client
+EthernetClient client;
+
+void setup()
+{
+  // Start Serial for debugging on the Serial Monitor
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  pinMode(sensor,INPUT);
+
+   Serial.println("Reading From the Sensor ...");
+   lcd.print("Moisture:");
+   lcd.print("Gas val:");
+   delay(2000);
+  
+  // Start Ethernet on Arduino
+  startEthernet();
+}
+
+void loop()
+{
+  // Read value from Analog Input Pin 0
+  String analogValue0 = String(analogRead(sensor_pin)*(4.76/1024), DEC);
+  lcd.setCursor(0, 1);
+   lcd.print(output_value);
+   lcd.print("%");
+
+   output_value= analogRead(sensor_pin);
+
+   output_value = map(output_value,550,0,0,100);
+
+   Serial.print("Mositure : ");
+   
+   Serial.print(output_value);
+   Serial.print("%");
+   Serial.print("   ");
+   
+   Serial.print("gas_value : ");
+   Serial.println(gas_value);
+
+   gas_value=analogRead(sensor);
+   
+   lcd.print("      ");
+   lcd.print(gas_value);
+
+   delay(1000);
+
+  
+  // Print Update Response to Serial Monitor
+  if (client.available())
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // Disconnect from ThingSpeak
+  if (!client.connected() && lastConnected)
+  {
+    Serial.println("...disconnected");
+    Serial.println();
+    
+    client.stop();
+  }
+  
+  // Update ThingSpeak
+  if(!client.connected() && (millis() - lastConnectionTime > updateThingSpeakInterval))
+  {
+    updateThingSpeak("field1="+analogValue0);
+  }
+  
+  // Check if Arduino Ethernet needs to be restarted
+  if (failedCounter > 3 ) {startEthernet();}
+  
+  lastConnected = client.connected();
+}
+
+void updateThingSpeak(String tsData)
+{
+  if (client.connect(thingSpeakAddress, 80))
+  {         
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+writeAPIKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+
+    client.print(tsData);
+    
+    lastConnectionTime = millis();
+    
+    if (client.connected())
+    {
+      Serial.println("Connecting to ThingSpeak...");
+      Serial.println();
+      
+      failedCounter = 0;
+    }
+    else
+    {
+      failedCounter++;
+  
+      Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");   
+      Serial.println();
+    }
+    
+  }
+  else
+  {
+    failedCounter++;
+    
+    Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");   
+    Serial.println();
+    
+    lastConnectionTime = millis(); 
+  }
+}
+
+void startEthernet()
+{
+  
+  client.stop();
+
+  Serial.println("Connecting Arduino to network...");
+  Serial.println();  
+
+  delay(1000);
+  
+  // Connect to network amd obtain an IP address using DHCP
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("DHCP Failed, reset Arduino to try again");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("Arduino connected to network using DHCP");
+    Serial.println();
+  }
+  
+  delay(1000);
+}
